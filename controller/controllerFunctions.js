b4021a60fc0a8dfbb0f4d3c5f2256e1fe96fee23
Ryan Mobley <ryan.mobley94@gmail.com>
2019-10-03 19:34:58 -0500

    added brewrey

--- controller/controllerFunctions.js
+++ controller/controllerFunctions.js
@@ -7,17 +7,14 @@
     },
         /*will calculate the average rating of the beer by taking the review scores from the reviews table
         and calcing their average*/ 
-    },
     this.beerReviews = async (beerName) =>{
         //lists reviews where review_beer = beerName
     },
     this.addReview = async (reviewObj) =>{
         //add review to table
         let result = await db.reviews.create({
-            review_beer: reviewObj.beer,
             review_rating: reviewObj.rating,
-            review_paragraph: reviewObj.paragraph,
-            review_author: reviewObj.author
+            review_paragraph: reviewObj.paragraph
         });
     }
     this.userReviews = async (user) =>{
--- models/beers.js
+++ models/beers.js
@@ -6,6 +6,8 @@ module.exports = function(sequelize, DataTypes) {
       validate:{ notEmpty: true}},
     beer_description: {type: DataTypes.STRING, len: [1,2000], allowNull: false,
       validate:{ notEmpty: true}},
+    brewrey: {type: DataTypes.STRING, len: [1,75], allowNull: false,
+      validate: {notEmpty: true}},
     avg_rating:{type: DataTypes.INTEGER, defaultValue: 5},
     createdAt: {type: DataTypes.DATE, defaultValue: sequelize.literal('CURRENT_TIMESTAMP')},
     updatedAt:{type:DataTypes.DATE, defaultValue: sequelize.literal('CURRENT_TIMESTAMP')}
