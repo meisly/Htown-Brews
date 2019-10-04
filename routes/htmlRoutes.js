46058d1ec42e4cd1a9399f3d70341104a4e504e0
Drew Reynolds <drew@sportscrm.com>
2019-10-03 20:48:42 -0500

    added sign up page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,16 @@ module.exports = function(app) {
     });
   });
 
+  // Signup Page
+  app.get("/signup", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("signup", {
+        msg: "Create an Account!",
+        examples: dbExamples
+      });
+    });
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
