cc51007e9ba9f8833bafd5856a201b7deb0c6ba9
Tierra <sr.undershaft@gmail.com>
2019-10-03 19:38:12 -0500

    i don't remember

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   // Materialize code to add data for autocomplete search.
   // Replace Data with our data from beers db
-  $('input.autocomplete').autocomplete({
+  $("input.autocomplete").autocomplete({
     data: {
       Apple: null,
       Microsoft: null,
