f2b8e76c1664b241d6f80a139cf704202f19280f
Ryan Mobley <ryan.mobley94@gmail.com>
2019-10-08 20:34:40 -0500

    fixed merge issue

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,29 +1,29 @@
-$(document).ready(function () {
-    $('.collapsible').collapsible();
-    function beerQuery(id) {
-        //queries reviews then queries their user in the getReviewsAuthors based on userId
-        $.ajax(`api/review/${id}`, {
-            type: "GET"
-        }).then(results => {
-            console.table(results);
-            results.forEach(index => {
-                getReviewAuthors(index);
-            });
-        });
+$(document).ready(function() {
+  $(".collapsible").collapsible();
+  function beerQuery(id) {
+    //queries reviews then queries their user in the getReviewsAuthors based on userId
+    $.ajax(`api/review/${id}`, {
+      type: "GET"
+    }).then(results => {
+      console.table(results);
+      results.forEach(index => {
+        getReviewAuthors(index);
+      });
     });
-function getReviewAuthors(data) {
+  }
+  function getReviewAuthors(data) {
     //once we have both username info and the review info we can append the review
     $.ajax(`api/user/${data.userId}`, {
-        type: "GET"
+      type: "GET"
     }).then(results => {
-        appendReviews(data, results.username);
+      appendReviews(data, results.username);
     });
-}
-function appendReviews(data, reviewAuthor) {
+  }
+  function appendReviews(data, reviewAuthor) {
     //creates the review div then appends it
     let reviewString = "<div class='reviewBlock'><div class='stars'></div>";
     reviewString += `<h2> By: ${reviewAuthor} </h2>`;
     reviewString += `<p> ${data.review_paragraph} </p>`;
     $(reviewString).appendTo(".reviews-list");
-}
-})
+  }
+});
