359d48fc007b73feba4a70232d00ca0ef48390ec
Tierra <sr.undershaft@gmail.com>
2019-10-12 22:18:52 -0500

    initalized redis

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
 
-var db = require("./models");
+const db = require("./models");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+//Session Storage
+
+const RedisStore = require('connect-redis')(session);
+
+if (process.env.REDIS_URL) {
+  let redis = require('redis').createClient(process.env.REDIS_URL);
+} else {
+  redis = require("redis").createClient();
+}
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -16,7 +25,11 @@ app.use(express.static("public"));
 app.use(
   session({
     secret: "keyboard cat",
-    cookie: { secure: true ,maxAge: 60000 },
+    cookie: { secure: true, maxAge: 60000 },
+    store:
+      process.env.NODE_ENV === "production"
+        ? new RedisStore({ url: process.env.REDIS_URL })
+        : new RedisStore({ client: redis }),
     saveUninitialized: true,
     resave: false
   })
@@ -44,8 +57,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
